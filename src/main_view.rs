22a136b5e888cf0efee6eca8ebba66879fa53173
Jack-Bell-Power <3452896431@qq.com>
2026-08-16 23:36:51 +0800

    Fix can't select the path mutiple times, cause the application close.

--- src/main_view.rs
+++ src/main_view.rs
@@ -5,7 +5,7 @@ use gpui_component::{
     h_flex,
     input::{Input, InputState},
 };
-use rfd::FileDialog;
+use rfd::{AsyncFileDialog, FileDialog};
 
 pub struct MainView {
     pal_path: Entity<InputState>,
@@ -15,6 +15,7 @@ pub struct MainView {
 
 impl Render for MainView {
     fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let pal_path = self.pal_path.clone();
         v_form()
             .gap(px(12.))
             .child(
@@ -23,25 +24,28 @@ impl Render for MainView {
                         .gap(px(8.))
                         .px(px(8.))
                         .child(Input::new(&self.pal_path))
-                        .child(
-                            Button::new("b_pal")
-                                .label("Open")
-                                .on_click(cx.listener(|this, _, window, cx| {
-                                    if let Some(path) = FileDialog::new()
+                        .child(Button::new("b_pal").label("Open").on_click(cx.listener(
+                            |_, _, window, cx| {
+                                cx.spawn_in(window, async |this, cx| {
+                                    if let Some(file) = AsyncFileDialog::new()
                                         .set_title("Select Palette File")
                                         .add_filter("Palette", &["pal"])
                                         .pick_file()
+                                        .await
                                     {
-                                        this.pal_path.update(cx, |state, cx| {
-                                        state.set_value(
-                                            path.to_string_lossy().to_string(),
-                                            window,
-                                            cx,
-                                        );
-                                    });
+                                        let path = file.path().to_string_lossy().to_string();
+                                        cx.update(|window, cx| {
+                                            this.update(cx, |this, cx| {
+                                                this.set_pal_path(path, window, cx);
+                                            })
+                                            .ok();
+                                        })
+                                        .ok();
                                     }
-                                })),
-                        ),
+                                })
+                                .detach();
+                            },
+                        ))),
                 ),
             )
             .child(
@@ -51,20 +55,25 @@ impl Render for MainView {
                         .px(px(8.))
                         .child(Input::new(&self.shp_path))
                         .child(Button::new("b_shp").label("Open").on_click(cx.listener(
-                            |this, _, window, cx| {
-                                if let Some(path) = FileDialog::new()
-                                    .set_title("Select Shape File")
-                                    .add_filter("Shape", &["shp"])
-                                    .pick_file()
-                                {
-                                    this.shp_path.update(cx, |state, cx| {
-                                        state.set_value(
-                                            path.to_string_lossy().to_string(),
-                                            window,
-                                            cx,
-                                        );
-                                    });
-                                }
+                            |_, _, window, cx| {
+                                cx.spawn_in(window, async |this, cx| {
+                                    if let Some(file) = AsyncFileDialog::new()
+                                        .set_title("Select Shape File")
+                                        .add_filter("Shape", &["shp"])
+                                        .pick_file()
+                                        .await
+                                    {
+                                        let path = file.path().to_string_lossy().to_string();
+                                        cx.update(|window, cx| {
+                                            this.update(cx, |this, cx| {
+                                                this.set_shp_path(path, window, cx);
+                                            })
+                                            .ok();
+                                        })
+                                        .ok();
+                                    }
+                                })
+                                .detach();
                             },
                         ))),
                 ),
@@ -78,9 +87,25 @@ impl Render for MainView {
                         .child(
                             Button::new("b_extraction")
                                 .label("Open")
-                                .on_click(|_, _, _| {
-                                    println!("extraction_path:");
-                                }),
+                                .on_click(cx.listener(|_, _, window, cx| {
+                                    cx.spawn_in(window, async |this, cx| {
+                                        if let Some(file) = AsyncFileDialog::new()
+                                            .set_title("Select extraction path")
+                                            .pick_folder()
+                                            .await
+                                        {
+                                            let path = file.path().to_string_lossy().to_string();
+                                            cx.update(|window, cx| {
+                                                this.update(cx, |this, cx| {
+                                                    this.set_extraction_path(path, window, cx);
+                                                })
+                                                .ok();
+                                            })
+                                            .ok();
+                                        }
+                                    })
+                                    .detach();
+                                })),
                         ),
                 ),
             )
@@ -111,5 +136,23 @@ impl MainView {
             shp_path,
             extraction_path,
         }
+    }
+
+    fn set_pal_path(&self, path: String, window: &mut Window, cx: &mut Context<Self>) {
+        self.pal_path.update(cx, |state, cx| {
+            state.set_value(path, window, cx);
+        });
+    }
+
+    fn set_shp_path(&self, path: String, window: &mut Window, cx: &mut Context<Self>) {
+        self.shp_path.update(cx, |state, cx| {
+            state.set_value(path, window, cx);
+        });
+    }
+
+    fn set_extraction_path(&self, path: String, window: &mut Window, cx: &mut Context<Self>) {
+        self.extraction_path.update(cx, |state, cx| {
+            state.set_value(path, window, cx);
+        });
     }
 }
